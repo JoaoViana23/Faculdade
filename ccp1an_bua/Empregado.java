e2b7ceb3d9471bd7a7b651bc25e3fc1f066901ff
Joao Guilherme <joaomedeiros.4180@aluno.saojudas.br>
2020-09-03 20:57:59 -0300

    Adicionando getter/setter para tipo. Instanciados 3 empregados na classe teste

--- ccp1an_bua/Empregado.java
+++ ccp1an_bua/Empregado.java
@@ -1,6 +1,29 @@
 public class Empregado {
     private String nome;
     private int idade;
+    private int tipo;
+    private double salario;
+    private double comissao;
+    private double bonus;
+
+    public double calculaSalario() {
+        switch (tipo) {
+            case 1:
+                return salario;
+            case 2: 
+                return salario + salario * comissao;
+            case 3:
+                return salario * bonus;
+            default:
+                return 0;
+        }
+    }
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+    public int getTipo() {
+        return tipo;
+    }
     public String getNome() {
         return nome;
     }
